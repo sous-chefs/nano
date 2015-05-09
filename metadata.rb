14a47404fcb6c34cf7b2cbe7d8c957789eb5652e
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2015-05-08 21:31:31 -0700

    bump version to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'joseph@josephholsten.com'
 license          'Apache 2.0'
 description      'Installs nano'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 %w(debian ubuntu arch redhat centos fedora scientific oracle amazon).each do |os|
   supports os
