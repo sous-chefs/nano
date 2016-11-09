0434c9037a966f044b64cbdc4a9468d7e0552894
Tim Smith <tsmith@chef.io>
2016-11-08 17:29:12 -0800

    Add chef_version metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,4 @@
 name 'nano'
-source_url "https://github.com/chef-brigade/#{name}-cookbook" if respond_to?(:source_url)
-issues_url "https://github.com/chef-brigade/#{name}-cookbook/issues" if respond_to?(:issues_url)
 maintainer 'Chef Brigade'
 maintainer_email 'help@chefbrigade.io'
 license 'Apache 2.0'
@@ -11,3 +9,7 @@ version '1.0.3'
 %w(debian ubuntu arch redhat centos fedora scientific oracle amazon).each do |os|
   supports os
 end
+
+source_url "https://github.com/chef-brigade/#{name}-cookbook" if respond_to?(:source_url)
+issues_url "https://github.com/chef-brigade/#{name}-cookbook/issues" if respond_to?(:issues_url)
+chef_version '>= 11.0' if respond_to?(:chef_version)
