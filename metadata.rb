eef6b9d96f801d50018e8a5bcc4adf3d862d8bd9
Tim Smith <tsmith@chef.io>
2018-02-19 12:31:32 -0800

    Use a SPDX compliant license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'nano'
 maintainer 'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs nano'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.3'
