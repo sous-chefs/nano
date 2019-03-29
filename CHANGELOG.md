59316cea6ed8da5eab6983917f5fa3681e68ffa6
Jason Field <jason@avon-lea.co.uk>
2019-03-29 16:52:47 +0000

    Add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# CHANGELOG for nano
+
+This file is used to list changes made in each version of nano.
+
+## Unreleased
+
+## 2.0.1 (2019-03-29)
+
+- Added changelog to cookbook
+
+## 2.0.0 (2019-03-29)
+
+- Removed existing recipe
+- Create new config resources to be able to configure nanorc files
+- Created new install resource
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures nano'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '2.0.0'
+version           '2.0.1'
 source_url        "https://github.com/sous-chefs/#{name}"
 issues_url        "https://github.com/sous-chefs/#{name}/issues"
 chef_version      '>= 13.0'
