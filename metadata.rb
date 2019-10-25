13268c48a3c8f67449da2c790172c855c7429239
Tim Smith <tsmith@chef.io>
2019-10-24 18:29:31 -0700

    Cookstyle fixes with Cookstyle 5.10 (#34)
    Autocorrects + cleanup opensuse
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,11 @@ maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures nano'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '2.0.1'
 source_url        "https://github.com/sous-chefs/#{name}"
 issues_url        "https://github.com/sous-chefs/#{name}/issues"
 chef_version      '>= 13.0'
 
-%w(debian ubuntu arch redhat centos fedora scientific oracle amazon suse opensuse opensuseleap).each do |os|
+%w(debian ubuntu arch redhat centos fedora scientific oracle amazon suse opensuseleap).each do |os|
   supports os
 end
