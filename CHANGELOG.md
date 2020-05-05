5974b994d7c47595256a2e7a70efdd2b1582c824
Jason Field <jason@avon-lea.co.uk>
2020-05-05 20:43:45 +0100

    release 2.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 This file is used to list changes made in each version of nano.
 
-## Unreleased
+## 2.0.2 (2020-05-05)
 
 - Removed Debian 8 from testing
 - Remove unnecessary long_description metadata from metadata.rb
--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,9 @@ maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures nano'
-version           '2.0.1'
-source_url        "https://github.com/sous-chefs/#{name}"
-issues_url        "https://github.com/sous-chefs/#{name}/issues"
+version           '2.0.2'
+source_url        "https://github.com/sous-chefs/nano"
+issues_url        "https://github.com/sous-chefs/nano/issues"
 chef_version      '>= 13.0'
 
 %w(debian ubuntu arch redhat centos fedora scientific oracle amazon suse opensuseleap).each do |os|
